ec8faeb24d0b89e19c1e567c65974b773d651869
Raman Sehgal <sc.ramansehgal@gmail.com>
2023-05-02 11:56:26 +0530

    Added first algorithm for waveform smoothing. Tested on the BAR data. The effect of smoothing can be seen using test/DSP/Test_SmoothWave.cpp program

--- inc/DSPAlgos.h
+++ inc/DSPAlgos.h
@@ -9,24 +9,27 @@
 #include <vector>
 
 class DSPAlgos {
-private:
-  std::vector<short> fWaveForm;
+public:
+  std::vector<short> *fWaveForm;
   unsigned long int fCoarseTStamp;
   unsigned long int fFineTStamp;
 
+private:
   // To be provided from constructor
   short fThreshold;
   unsigned short fPretrigger;
   unsigned short fBaseline;
   unsigned short fShortGate;
   unsigned short fLongGate;
+  unsigned short fWindowSize;
 
 public:
   DSPAlgos();
+  DSPAlgos(unsigned short windowSize);
   DSPAlgos(short threshold, unsigned short preTrigger, unsigned short baseline, unsigned short shortGate,
-           unsigned short longGate);
+           unsigned short longGate,unsigned short windowSize);
   // Required Processors
-  void SmoothenSignal();
+  std::vector<short> SmoothenSignal();
   void CalculateBaseline();
   void CalculateCFD();
   void CalculateIntegratedCharge();
@@ -39,6 +42,7 @@ public:
   unsigned short GetShortGate()const;
   unsigned short GetLongGate()const;
   short GetThreshold()const;
+  unsigned short GetWindowSize()const;
 };
 
 #endif
--- src/DSPAlgos.cpp
+++ src/DSPAlgos.cpp
@@ -6,12 +6,53 @@
 #include "DSPAlgos.h"
 #include <iostream>
 
-DSPAlgos::DSPAlgos() {}
+DSPAlgos::DSPAlgos() {
+  fWaveForm = nullptr;
+  fWindowSize = 10;
+}
+
+DSPAlgos::DSPAlgos(unsigned short windowSize) : fWindowSize(windowSize) { fWaveForm = nullptr; }
 
 DSPAlgos::DSPAlgos(short threshold, unsigned short preTrigger, unsigned short baseline, unsigned short shortGate,
-                   unsigned short longGate)
-    : fThreshold(threshold), fPretrigger(preTrigger), fBaseline(baseline), fShortGate(shortGate), fLongGate(longGate) {}
+                   unsigned short longGate, unsigned short windowSize)
+    : fThreshold(threshold), fPretrigger(preTrigger), fBaseline(baseline), fShortGate(shortGate), fLongGate(longGate),
+      fWindowSize(windowSize) {
+  fWaveForm = nullptr;
+}
 
+// Required Processors
+std::vector<short> DSPAlgos::SmoothenSignal() {
+  std::vector<short> tWaveForm;
+  for (unsigned int i = 0; i < fWaveForm->size(); i++) {
+    tWaveForm.push_back(fWaveForm->at(i));
+  }
+  std::vector<short> smoothed_signal(fWaveForm->size());
+
+  // Loop over each point in the signal
+  for (int i = 0; i < tWaveForm.size(); i++) {
+    double sum = 0.0;
+    int count = 0;
+
+    // Loop over the data points in the moving average window centered on the current point
+    for (int j = i - fWindowSize / 2; j <= i + fWindowSize / 2; j++) {
+      // Check if the current window position is within the bounds of the signal
+      if (j >= 0 && j < tWaveForm.size()) {
+        sum += tWaveForm[j];
+        count++;
+      }
+    }
+    // Calculate the average of the data points in the window and store in the smoothed signal
+    // std::cout << "I : " << i << " : Sum : " << sum << " : Count : " << count << std::endl;
+    smoothed_signal[i] = sum / count;
+    tWaveForm[i] = smoothed_signal[i];
+  }
+  return smoothed_signal;
+}
+void DSPAlgos::CalculateBaseline() {}
+void DSPAlgos::CalculateCFD() {}
+void DSPAlgos::CalculateIntegratedCharge() {}
+
+// Required Getters
 unsigned long int DSPAlgos::GetCoarseTStamp() const { return fCoarseTStamp; }
 unsigned long int DSPAlgos::GetFineTStamp() const { return fFineTStamp; }
 unsigned short DSPAlgos::GetPreTrigger() const { return fPretrigger; }
@@ -19,3 +60,4 @@ unsigned short DSPAlgos::GetBaseline() const { return fBaseline; }
 unsigned short DSPAlgos::GetShortGate() const { return fShortGate; }
 unsigned short DSPAlgos::GetLongGate() const { return fLongGate; }
 short DSPAlgos::GetThreshold() const { return fThreshold; }
+unsigned short DSPAlgos::GetWindowSize() const { return fWindowSize; }
